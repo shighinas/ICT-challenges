46bbacf8699667400746203a1faff9c81ac611fb
shighinas <61883009+shighinas@users.noreply.github.com>
2020-12-10 12:56:04 +0530

    Add files via upload
    ShiLib : The Library app

---
+++ ShiLib_library_app/src/routes/authorsroutes.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const authorsrouter = express.Router();
+function router(nav){
+    var authors = [
+        {
+            title: "Joseph barbera",
+            book: "Tom &jerry",
+            genre: "Cartoon",
+            img: "joe.jpeg"
+        },
+        {
+            book: "Harry potter",
+            title: "J K Rowling",
+            genre: "Fantacy",
+            img: "jk.jpg"
+        },
+        {
+            book: "Pathummayude Aadu",
+            title: "Vaikam Muhammed Basheer",
+            genre: "Drama",
+            img: "basheer.jpg"
+        }
+    ];
+    authorsrouter.get('/', function(req, res){
+        res.render('authors',
+            {
+                nav,
+                title: 'ShiLib: The LIbrary',
+                authors
+            });
+    });
+    authorsrouter.get('/:id', function(req, res){
+        const id = req.params.id;
+        if(id==='new'){
+            res.render('newauthor', {nav, title: 'ShiLib: The LIbrary'});
+        }
+        else {
+            author = authors[id];
+            res.render('author',
+                {
+                    nav,
+                    title: 'ShiLib: The LIbrary',
+                    author
+                });
+        }
+    });
+    return authorsrouter;
+}
+
+
+module.exports = router;
